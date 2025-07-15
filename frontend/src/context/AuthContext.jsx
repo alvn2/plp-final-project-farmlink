939e5fb54a481e480aff0c1a67c03db6b05e856c
alvn <alvnmugo@gmail.com>
2025-07-15 22:02:06 +0300

    update

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -14,7 +14,7 @@ export const useAuth = () => {
 };
 
 // Configure axios defaults
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://plp-final-project-farmlink.onrender.com ';
 
 // Auth Provider Component
 export const AuthProvider = ({ children }) => {
